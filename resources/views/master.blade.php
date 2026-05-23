1eb28406c2b2bc59132a233629d5696aea8c97e4
rindialvionita888-svg <rindialvionita888@gmail.com>
2026-05-23 13:33:33 +0700

    update master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,23 +1,43 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RindiAlv | @yield('title')</title>
+    <title>Portofolio - RindiAlv</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('style.css') }}">
-    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;700&display=swap" rel="stylesheet">
+    
+    <style>
+    
+        body {
+            font-family: 'Space+Grotesk', sans-serif;
+            background-color: #121212;
+            overflow-x: hidden;
+        }
+        .app-container {
+            display: flex;
+            min-height: 100vh;
+        }
+        .main-content {
+            flex-grow: 1;
+            background-color: #121212;
+            padding-left: 20px;
+            padding-right: 20px;
+        }
+        .text-accent {
+            color: #bc13fe;
+        }
+    </style>
 </head>
+<body class="text-white">
 
-<body class="d-flex flex-column min-vh-100 bg-dark text-white">
+    <div class="app-container">
+        @include('navbar')
 
-    @include('navbar')
-
-    <div class="main-wrapper flex-grow-1">
-        @yield('content')
+        <main class="main-content">
+            @yield('content')
+        </main>
     </div>
-
-   @include('footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
