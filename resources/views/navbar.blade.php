4a705d7fc193a6ee73c72b1853a10d9f0cbab422
rindialvionita888-svg <rindialvionita888@gmail.com>
2026-05-23 13:20:33 +0700

    update navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -3,14 +3,18 @@
         <h2 class="fw-bold">R<span class="text-accent">A</span></h2>
     </div>
     <div class="nav-links">
-       <a href="{{ route('home') }}" class="nav-item-link">Intro</a>
-        <a href="#projects" class="nav-item-link">Works</a>
+        <a href="{{ route('home') }}" class="nav-item-link">Intro</a>
+        
+        <a href="/portofolio" class="nav-item-link">Works</a>
+        
         <a href="#services" class="nav-item-link">Services</a>
-       <a href="{{ route('detail') }}" class="nav-item-link">Archive</a>
+        <a href="{{ route('detail') }}" class="nav-item-link">Archive</a>
+        
+        <a href="/portofolio" class="nav-item-link">PROJECT</a>
     </div>
     <div class="nav-footer">
-        <button class="btn-admin" data-bs-toggle="modal" data-bs-target="#crudModal">
+        <a href="/portofolio/create" class="btn border-0 text-decoration-none p-0" title="Tambah Project Baru">
             <span style="font-size: 1.5rem;">⚙️</span>
-        </button>
+        </a>
     </div>
 </nav>
