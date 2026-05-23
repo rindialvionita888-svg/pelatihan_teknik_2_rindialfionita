9e1604e9bc4a82cf7e7d2d3fa5a1c586d5ad9855
rindialvionita888-svg <rindialvionita888@gmail.com>
2026-05-23 14:09:39 +0700

    update portofolio php

--- app/Models/Portofolio.php
+++ app/Models/Portofolio.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portofolio extends Model
 {
-    protected $fillable = ['judul', 'deskripsi', 'link_project'];
+   protected $fillable = ['judul', 'deskripsi', 'gambar', 'link_project'];
 }
