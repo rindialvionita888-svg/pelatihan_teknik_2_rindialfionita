9c7d87a475736dc205c1b75b7b0ef6d52baa7cc4
rindialvionita888-svg <rindialvionita888@gmail.com>
2026-05-23 14:07:17 +0700

    buat migrate

---
+++ database/migrations/2026_05_23_070405_add_gambar_to_portofolio_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint; // <-- Ini yang menjamin Blueprint aman terbaca
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('portofolios', function (Blueprint $table) {
+          
+            $table->string('gambar')->nullable()->after('deskripsi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('portofolios', function (Blueprint $table) {
+           
+            $table->dropColumn('gambar');
+        });
+    }
+};
