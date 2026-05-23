7d5019a0e541bae03a1b09a65e9f20aaabec922a
rindialvionita888-svg <rindialvionita888@gmail.com>
2026-05-23 14:08:09 +0700

    Update 2026_05_23_070405_add_gambar_to_portofolio_table.php

--- database/migrations/2026_05_23_070405_add_gambar_to_portofolio_table.php
+++ database/migrations/2026_05_23_070405_add_gambar_to_portofolio_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint; // <-- Ini yang menjamin Blueprint aman terbaca
+use Illuminate\Database\Schema\Blueprint; 
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
